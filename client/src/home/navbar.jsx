56d6b48ecbb6d741311b446e4d774845b27eaa4d
Masood Ahmadi <masoodahmadi553@gmail.com>
2023-11-25 12:59:26 +0200

    login is added

--- client/src/home/navbar.jsx
+++ client/src/home/navbar.jsx
@@ -10,7 +10,7 @@ function Navbars() {
   return (
     <Navbar expand='lg' className='bg-body-tertiary'>
       <Container>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href='/'>E-commerce application</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
